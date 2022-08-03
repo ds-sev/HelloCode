cf246087b25b24e61848cf246b96062c7a38fa1c
Dmitry L <ds-sev@ya.ru>
2022-08-03 21:11:54 +0300

    add Example 008 Homework(dog and friends)

---
+++ Example008_homework/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        int time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count = count + 1;
+    }
+    if (friend == 2)
+    {
+        int time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count = count + 1;
+    }
+        
+}
+Console.WriteLine("Собака пробежит " + count + " раз");
+
