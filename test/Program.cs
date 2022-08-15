5720429072260485c42f3320faf9c89833f59ce8
Dmitry L <ds-sev@ya.ru>
2022-08-15 11:57:02 +0300

    simple tasks about Methods

---
+++ test/Program.cs
@@ -0,0 +1,17 @@
+﻿int MyMethod(int a, int b)
+{
+    
+    int result;
+    result = a + b;
+    return result;
+}
+
+Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Summ of this numbers: ");
+
+int res = MyMethod(a, b);
+Console.WriteLine(res);
